71459cce3553665417da2e6196f33366558d89fe
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2023-03-23 11:23:17 +0100

    test brew

---
+++ snitch.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Snitch < Formula
+  desc "Intrusion detection"
+  homepage "snitch.cool"
+  url "https://github.com/HerrMuellerluedenscheid/snitch/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "55b9c887557321cf6b2f6f879763769d1b49374b608719b1160edcae88516aee"
+  license "MIT"
+
+  depends_on "rust" => :install
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cargo" "build"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test snitch`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "target/debug/snitch"
+  end
+end
