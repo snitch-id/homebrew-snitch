05c16491601f7bc77c9683f6ed7480d91ae0eeaa
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2023-03-29 07:53:09 +0200

    update release

--- snitch.rb
+++ snitch.rb
@@ -1,7 +1,7 @@
 class Snitch < Formula
-  desc "Intrusion detection"
+  desc "Intrusion detection and filesystem integrity monitoring."
   homepage "snitch.cool"
-  url "https://github.com/HerrMuellerluedenscheid/snitch/releases/download/v0.1.6/snitch-osx"
+  url "https://github.com/snitch-id/snitch/releases/download/v0.1.7/snitch-osx"
   sha256 "1eed33150fb177b9a1c547cd73858648cfdc4247b001b3a29b824bd6ede6f137"
   license "MIT"
 
