d22e32ccc3c8ffdb42f95803d21386be8a63694f
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2023-03-29 11:20:44 +0200

    fix sha

--- snitch.rb
+++ snitch.rb
@@ -2,7 +2,7 @@ class Snitch < Formula
   desc "Intrusion detection and filesystem integrity monitoring."
   homepage "snitch.cool"
   url "https://github.com/snitch-id/snitch/releases/download/v0.1.7/snitch-osx"
-  sha256 "1eed33150fb177b9a1c547cd73858648cfdc4247b001b3a29b824bd6ede6f137"
+  sha256 "3a3cff7ffbb060c5a156ed2ad6ea58a7a6dfacb5b1230f8f59edc99eb3e048bf"
   license "MIT"
 
   def install
