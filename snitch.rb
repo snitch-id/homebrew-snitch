a42d2c50ba70d007a1fb91f2c0eef6daa624aa4d
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2023-04-11 23:04:38 +0200

    bump version

--- snitch.rb
+++ snitch.rb
@@ -1,8 +1,8 @@
 class Snitch < Formula
-  desc "Intrusion detection and filesystem integrity monitoring."
-  homepage "snitch.cool"
-  url "https://github.com/snitch-id/snitch/releases/download/v0.1.7/snitch-osx"
-  sha256 "3a3cff7ffbb060c5a156ed2ad6ea58a7a6dfacb5b1230f8f59edc99eb3e048bf"
+  desc "Intrusion detection system and filesystem integrity monitoring."
+  homepage "https://snitch.cool"
+  url "https://github.com/HerrMuellerluedenscheid/snitch/releases/download/v0.1.9/snitch-osx"
+  sha256 "e7bdcdee884efab5f7eaeb6fa202b3d705930cc72083812bd085ef5228548bc6"
   license "MIT"
 
   def install
